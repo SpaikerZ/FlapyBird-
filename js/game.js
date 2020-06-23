a27212a6a42695c653009ee94c9a253c13eaccff
SpaikerZ <egori228337@gmail.com>
2020-06-23 22:45:21 +0300

    finish 2 commit

--- js/game.js
+++ js/game.js
@@ -1,14 +1,14 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
-var bird = new Image();
-var bg = new Image();
+var bax3 = new Image();
+var bg2 = new Image();
 var fg = new Image();
 var pipeUp = new Image();
 var pipeBottom = new Image();
 
-bird.src = "img/bird.png";
-bg.src = "img/bg.png";
+bax3.src = "img/bax3.png";
+bg2.src = "img/bg2.png";
 fg.src = "img/fg.png";
 pipeUp.src = "img/pipeUp.png";
 pipeBottom.src = "img/pipeBottom.png";
@@ -20,13 +20,13 @@ var score_audio = new Audio();
 fly.src = "audio/fly.mp3";
 score_audio.src = "audio/score.mp3";
 
-var gap = 90;
+var gap = 110;
 
 // При нажатии на какую-либо кнопку
 document.addEventListener("keydown", moveUp);
 
 function moveUp() {
- yPos -= 25;
+ yPos -= 30;
  fly.play();
 }
 
@@ -42,10 +42,10 @@ var score = 0;
 // Позиция птички
 var xPos = 10;
 var yPos = 150;
-var grav = 1.5;
+var grav = 2;
 
 function draw() {
- ctx.drawImage(bg, 0, 0);
+ ctx.drawImage(bg2, 0, 0);
 
  for(var i = 0; i < pipe.length; i++) {
  ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
@@ -61,10 +61,10 @@ function draw() {
  }
 
  // Отслеживание прикосновений
- if(xPos + bird.width >= pipe[i].x
+ if(xPos + bax3.width >= pipe[i].x
  && xPos <= pipe[i].x + pipeUp.width
  && (yPos <= pipe[i].y + pipeUp.height
- || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
+ || yPos + bax3.height >= pipe[i].y + pipeUp.height + gap) || yPos + bax3.height >= cvs.height - fg.height) {
  location.reload(); // Перезагрузка страницы
  }
 
@@ -75,7 +75,7 @@ function draw() {
  }
 
  ctx.drawImage(fg, 0, cvs.height - fg.height);
- ctx.drawImage(bird, xPos, yPos);
+ ctx.drawImage(bax3, xPos, yPos);
 
  yPos += grav;
 
